89d0c1ecc94c6732b4bf53ebcf98dfbea12d8e8a
Quang Le <quanghuyle3@gmail.com>
2023-06-19 15:03:07 -0700

    first commit

---
+++ src/main/java/com/dbmanagement/GymLife/GymLifeApplication.java
@@ -0,0 +1,13 @@
+package com.dbmanagement.GymLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbmanagement/GymLife/GymLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbmanagement.GymLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
