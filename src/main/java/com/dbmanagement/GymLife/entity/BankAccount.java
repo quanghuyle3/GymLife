21cf8d2cb107d0a2d64a8ce0b90040676b973b15
Quang Le <quanghuyle3@gmail.com>
2023-06-30 16:10:22 -0700

    Add first entity

---
+++ src/main/java/com/dbmanagement/GymLife/entity/BankAccount.java
@@ -0,0 +1,68 @@
+package com.dbmanagement.GymLife.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class BankAccount {
+
+    @Id
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "bank_name")
+    private String bankName;
+
+    @Column(name = "routine_number")
+    private int routineNumber;
+
+    @Column(name = "remaining_balance_owed")
+    private double remainingBalanceOwed;
+
+    public BankAccount() {
+    }
+
+    public BankAccount(String accountNumber, String bankName, int routineNumber) {
+        this.accountNumber = accountNumber;
+        this.bankName = bankName;
+        this.routineNumber = routineNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getRoutineNumber() {
+        return routineNumber;
+    }
+
+    public void setRoutineNumber(int routineNumber) {
+        this.routineNumber = routineNumber;
+    }
+
+    public double getRemainingBalanceOwed() {
+        return remainingBalanceOwed;
+    }
+
+    public void setRemainingBalanceOwed(double remainingBalanceOwed) {
+        this.remainingBalanceOwed = remainingBalanceOwed;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount [accountNumber=" + accountNumber + ", bankName=" + bankName + ", routineNumber="
+                + routineNumber + ", remainingBalanceOwed=" + remainingBalanceOwed + "]";
+    }
+
+}
