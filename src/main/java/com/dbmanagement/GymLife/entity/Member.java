13a0d23dcc158303bfb182220398b22df3b3ae27
Quang Le <quanghuyle3@gmail.com>
2023-07-31 14:34:47 -0700

    Add Member entity

---
+++ src/main/java/com/dbmanagement/GymLife/entity/Member.java
@@ -0,0 +1,221 @@
+package com.dbmanagement.GymLife.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.REFRESH })
+    @JoinColumn(name = "membership_type")
+    private Membership membershipType; // no need to automatically create membership in db when create member
+
+    @Column(name = "date_join")
+    private String dateJoin;
+
+    @Column(name = "date_expiration")
+    private String dateExpiration;
+
+    @Column(name = "active")
+    private boolean active = true;
+
+    @OneToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH })
+    @JoinColumn(name = "bank_account_number")
+    private BankAccount bankAccountNumber;
+
+    public Member() {
+    }
+
+    public Member(String email, String userName, String password, String firstName, String lastName,
+            String address, String phoneNumber, String dateOfBirth, String gender,
+            String dateJoin, BankAccount bankAccountNumber) {
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.dateJoin = dateJoin;
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Membership getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(Membership membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public String getDateJoin() {
+        return dateJoin;
+    }
+
+    public void setDateJoin(String dateJoin) {
+        this.dateJoin = dateJoin;
+    }
+
+    public String getDateExpiration() {
+        return dateExpiration;
+    }
+
+    public void setDateExpiration(String dateExpiration) {
+        this.dateExpiration = dateExpiration;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public BankAccount getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(BankAccount bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Member [id=" + id + ", email=" + email + ", userName=" + userName +
+                ", password=" + password
+                + ", firstName=" + firstName + ", lastName=" + lastName + ", address=" +
+                address + ", phoneNumber="
+                + phoneNumber + ", dateOfBirth=" + dateOfBirth + ", gender=" + gender +
+                ", membership_type="
+                + membershipType + ", date_join=" + dateJoin + ", date_expiration=" +
+                dateExpiration + ", active="
+                + active + ", bankAccountNumber=" + bankAccountNumber + "]";
+    }
+
+}
