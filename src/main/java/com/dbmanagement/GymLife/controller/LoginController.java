eba5cd2adef7a84d3a071562f115f002d3f547d0
Quang Le <quanghuyle3@gmail.com>
2023-08-05 15:57:29 -0700

    Add Login Controller

---
+++ src/main/java/com/dbmanagement/GymLife/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.dbmanagement.GymLife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
