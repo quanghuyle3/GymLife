3253dfc330e15780558f64316421b322bb202581
Quang Le <quanghuyle3@gmail.com>
2023-08-05 16:04:34 -0700

    Add main controller for the application

--- src/main/java/com/dbmanagement/GymLife/controller/MainController.java
+++ src/main/java/com/dbmanagement/GymLife/controller/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("member")
+    public String showMember() {
+        return "member";
+    }
 }
