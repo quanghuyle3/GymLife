740356006df12f7ac873fc106479d8cf08a28a0e
Quang Le <quanghuyle3@gmail.com>
2023-08-04 19:07:26 -0700

    Add first Controller

---
+++ src/main/java/com/dbmanagement/GymLife/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.dbmanagement.GymLife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
