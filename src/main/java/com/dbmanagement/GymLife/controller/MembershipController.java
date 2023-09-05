929d4acfc8362ce65b5e7f0a91d258276ab2a1ba
Quang Le <quanghuyle3@gmail.com>
2023-09-05 11:02:52 -0700

    Add membership controller

---
+++ src/main/java/com/dbmanagement/GymLife/controller/MembershipController.java
@@ -0,0 +1,42 @@
+package com.dbmanagement.GymLife.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dbmanagement.GymLife.dao.AppDAO;
+import com.dbmanagement.GymLife.dao.MemberDAO;
+import com.dbmanagement.GymLife.dao.RoleDAO;
+import com.dbmanagement.GymLife.entity.Membership;
+
+@Controller
+@RequestMapping("/memberships")
+public class MembershipController {
+
+    public AppDAO appDAO;
+    public MemberDAO memberDAO;
+    public RoleDAO roleDAO;
+
+    @Autowired
+    public MembershipController(AppDAO appDAO, MemberDAO memberDAO, RoleDAO roleDAO) {
+        this.appDAO = appDAO;
+        this.memberDAO = memberDAO;
+        this.roleDAO = roleDAO;
+    }
+
+    @GetMapping("/retrieve")
+    public String retrieveAllMemberships(Model theModel) {
+
+        // List<Training> allTraining = appDAO.retrieveAllTraining();
+        List<Membership> allMemberships = appDAO.retrieveAllMembership();
+
+        theModel.addAttribute("memberships", allMemberships);
+
+        return "memberships-retrieve";
+    }
+
+}
