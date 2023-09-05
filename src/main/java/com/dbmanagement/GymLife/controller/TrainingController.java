b79bd71bdabb5c28c8007c5fc64b6bd7ffbdd871
Quang Le <quanghuyle3@gmail.com>
2023-09-05 11:02:28 -0700

    Add training controller

---
+++ src/main/java/com/dbmanagement/GymLife/controller/TrainingController.java
@@ -0,0 +1,41 @@
+package com.dbmanagement.GymLife.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dbmanagement.GymLife.dao.AppDAO;
+import com.dbmanagement.GymLife.dao.MemberDAO;
+import com.dbmanagement.GymLife.dao.RoleDAO;
+import com.dbmanagement.GymLife.entity.Training;
+
+@Controller
+@RequestMapping("/trainings")
+public class TrainingController {
+
+    public AppDAO appDAO;
+    public MemberDAO memberDAO;
+    public RoleDAO roleDAO;
+
+    @Autowired
+    public TrainingController(AppDAO appDAO, MemberDAO memberDAO, RoleDAO roleDAO) {
+        this.appDAO = appDAO;
+        this.memberDAO = memberDAO;
+        this.roleDAO = roleDAO;
+    }
+
+    @GetMapping("/retrieve")
+    public String retrieveAllTrainings(Model theModel) {
+
+        List<Training> allTraining = appDAO.retrieveAllTraining();
+
+        theModel.addAttribute("trainings", allTraining);
+
+        return "trainings-retrieve";
+    }
+
+}
