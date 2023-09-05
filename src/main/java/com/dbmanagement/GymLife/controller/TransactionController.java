ab040a76678cff42d51f07229a2845a59f6fffe8
Quang Le <quanghuyle3@gmail.com>
2023-09-05 11:02:19 -0700

    Add transaction controller

---
+++ src/main/java/com/dbmanagement/GymLife/controller/TransactionController.java
@@ -0,0 +1,42 @@
+package com.dbmanagement.GymLife.controller;
+
+import java.util.List;
+
+import com.dbmanagement.GymLife.entity.Transaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dbmanagement.GymLife.dao.AppDAO;
+import com.dbmanagement.GymLife.dao.MemberDAO;
+import com.dbmanagement.GymLife.dao.RoleDAO;
+
+@Controller
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    public AppDAO appDAO;
+    public MemberDAO memberDAO;
+    public RoleDAO roleDAO;
+
+    @Autowired
+    public TransactionController(AppDAO appDAO, MemberDAO memberDAO, RoleDAO roleDAO) {
+        this.appDAO = appDAO;
+        this.memberDAO = memberDAO;
+        this.roleDAO = roleDAO;
+    }
+
+    @GetMapping("/retrieve")
+    public String retrieveAllTransactions(Model theModel) {
+
+        List<Transaction> allTransactions = appDAO.retrieveAllTransaction();
+
+        theModel.addAttribute("transactions", allTransactions);
+
+        return "transactions-retrieve";
+    }
+
+}
